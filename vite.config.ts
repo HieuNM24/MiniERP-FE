5fec849bbf10a39d5cbc32b636aa8ba01da5fca8
Nguyen Minh Hieu <hieunguyentb24@gmail.com>
2026-08-03 14:31:06 +0700

    npthing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    open: true, // 👈 THÊM DÒNG NÀY: Tự động bật Chrome/Edge ngoài khi gõ npm run dev
+  },
+});
